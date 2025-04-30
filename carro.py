3a1694ae777052b70d56b6561238a00bd7f55d1d
GV <macarraocomo@gmail.com>
2025-04-30 15:28:37 -0300

    Add files via upload

---
+++ carro.py
@@ -0,0 +1,13 @@
+Cliente1 = "Carlos"
+Cliente2 = "Marcos"
+
+Carro1 = "BMW M4"
+Carro2 = "Porshe Carrera S"
+
+Marca1 = "BMW"
+Marca2 = "Porshe"
+
+Valor1 = "1.399.950,00"
+Valor2 = "1.080.000,00" 
+
+print (f"{Cliente1} temos uma {Carro1} da {Marca1} no valor de {Valor1}\n\n{Cliente2} temos uma {Carro2} da {Marca2} no valor de {Valor2}")
